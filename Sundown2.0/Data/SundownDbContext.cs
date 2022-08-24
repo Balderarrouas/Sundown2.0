2b27d2a7d3fba541693e2a3c987047eb96082868
balderarrouas <balder.arrouas@gmail.com>
2022-08-24 13:07:19 +0200

    Add project files.

---
+++ Sundown2.0/Data/SundownDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sundown2._0.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sundown2._0.Data
+{
+    public class SundownDbContext :DbContext
+    {
+        public SundownDbContext(DbContextOptions<SundownDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Astronaut> Astronauts { get; set; }
+
+
+    }
+}
